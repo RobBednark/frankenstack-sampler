cd6adec86bf96fafe2c1978d80ba704886cf9220
Rob Bednark <rbednark@gmail.com>
2025-06-05 10:53:27 -0700

    nginx/default.conf: create /frank-web-app-main-dev proxy frank-web-app-main-dev: Dockerfile: change CMD to "npm run dev" rename: add "dev" : frank-web-app-main > frank-web-app-main-dev rename: frank-main-api > frank-api-main

--- frank-api-main/frank_api_main.py
+++ frank-api-main/frank_api_main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/frank-api-main/')
+def frank_api_main():
+    return 'frank-api-main: success response'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
